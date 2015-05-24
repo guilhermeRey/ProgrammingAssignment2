33dbfb6b70bdbb59049c2198e1967dd8c10caeb8
Guilherme Rey <poli.rey@gmail.com>
2015-05-24 03:13:51 -0300

    Solution for cached matrix (Programming Assignment 2)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##  makeCacheMatrix(this)
+##    this: a matrix() object
+##  The function wraps a matrix object that is capable of:
+##    get/set it's matrix;
+##    cache it's inverse matrix
+##    return it's inverse matrix, that is cached
+makeCacheMatrix <- function(this = matrix()) {
+  this_inverse <- NULL
+  
+  get <- function () this
+  set <- function (newMatrix) {
+    x <<- newMatrix
+    this_inverse <<- NULL # flush cached attribute
+  }
+  
+  setInverse <- function (inverse) this_inverse <<- inverse
+  getInverse <- function () this_inverse
+  
+  list(get = get, set = set, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve(x, ...)
+##  x: a matrix object
+##  returns: x inverse matrix
+## The function ensures that x is solved and cached, returning it.
+##
+cacheSolve <- function(m, ...) {
+  inverse <- m$getInverse()
+  # So it is null? Let's solve and cache it!
+  if(is.null(inverse)) {
+    message("INFO: Calculating and caching...")
+    matrix <- m$get()
+    m$setInverse(solve(matrix, ...)) 
+  }
+  
+  m$getInverse() # always return the solved matrix :)
 }
